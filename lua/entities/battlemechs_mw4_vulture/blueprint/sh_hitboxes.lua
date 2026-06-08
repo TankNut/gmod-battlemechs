b1554c34080fb99803eefb2f25190756b0a2d5c1
TankNut <m.vlasblom@live.nl>
2026-06-09 00:06:45 +0200

    Update sh_hitboxes.lua

--- lua/entities/battlemechs_mw4_vulture/blueprint/sh_hitboxes.lua
+++ lua/entities/battlemechs_mw4_vulture/blueprint/sh_hitboxes.lua
@@ -29,11 +29,11 @@ if SERVER then
 end
 
 function ENT:BuildDamageGroups()
-	self:AddDamageGroup("Torso", 10, {"Torso"})
+	self:AddDamageGroup("Torso", 10, {"Torso", "Chin", "ChinGun"})
 
 	self:AddDamageGroup("Left Weapon", 3, {"LeftWeapon"})
 	self:AddDamageGroup("Right Weapon", 3, {"RightWeapon"})
 
 	self:AddDamageGroup("Left Leg", 4, {"LeftHip", "LeftKnee", "LeftFoot"})
-	self:AddDamageGroup("Right Leg ", 4, {"RightHip", "RightKnee", "RightFoot"})
+	self:AddDamageGroup("Right Leg", 4, {"RightHip", "RightKnee", "RightFoot"})
 end
