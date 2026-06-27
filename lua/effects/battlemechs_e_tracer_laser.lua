e1a780f02a452a1b9546f403a810d288456e91b0
TankNut <m.vlasblom@live.nl>
2026-06-27 03:57:01 +0200

    Overhaul laser tracers

--- lua/effects/battlemechs_e_tracer_laser.lua
+++ lua/effects/battlemechs_e_tracer_laser.lua
@@ -1,43 +1,64 @@
 local material = Material("battlemechs/trails/physbeam_white")
 
 function EFFECT:Init(data)
+	self.Time = 0
+
 	self.Entity = data:GetEntity()
 	self.Index = data:GetAttachment()
 
+	self.Trace = self.Entity:GetTrace()
 	self.Start = self.Entity:GetTracerOrigin(self.Index)
-	self.End = self.Entity:GetTrace(self.Index).HitPos
 
 	self.Material = material
 	self.Scale = 16
 	self.Brightness = 3
 	self.Color = Color(33, 255, 0)
+	self.PulseRatio = 1
+
+	self.Range = 56756
+	self.Delay = 0.1
+
+	self.Entity:SetupLaser(self)
 end
 
 function EFFECT:Think()
-	local should = self.Entity["GetIsFiring" .. self.Index](self.Entity)
-
-	if not should then
+	if not self.Entity:IsValid() or self.Entity:GetFireDuration() == 0 then
 		return false
 	end
 
+	self.Trace = self.Entity:GetTrace()
 	self.Start = self.Entity:GetTracerOrigin(self.Index)
-	self.End = self.Entity:GetTrace(self.Index).HitPos
 
-	self:SetPos(self.End)
-	self:SetRenderBoundsWS(self.Start, self.End)
+	self:SetPos(self.Trace.HitPos)
+	self:SetRenderBoundsWS(self.Start, self.Trace.HitPos)
 
 	return true
 end
 
 function EFFECT:Render()
-	local length = (self.Start - self.End):Length()
+	self.Time = self.Time + FrameTime()
+
+	if (self.Time % self.Delay) / self.Delay > self.PulseRatio then
+		return
+	end
+
+	local length = (self.Start - self.Trace.HitPos):Length()
 	local texcoord = math.Rand(0, 1)
 
 	render.SetMaterial(self.Material)
 
 	for i = 0, math.ceil(self.Brightness) - 1 do
-		self.Color.a = math.min((self.Brightness - i) * 255, 255)
+		render.StartBeam(2)
+			local brightness = (self.Brightness - i) * 255
 
-		render.DrawBeam(self.Start, self.End, self.Scale, texcoord, texcoord + length / 1024, self.Color)
+			self.Color.a = math.min(brightness, 255)
+			render.AddBeam(self.Start, self.Scale, texcoord, self.Color)
+
+			-- Decrease brightness as we approach our max range
+			brightness = (1 - self.Trace.Fraction) * brightness
+
+			self.Color.a = math.min(brightness, 255)
+			render.AddBeam(self.Trace.HitPos, self.Scale, texcoord + length / 1024, self.Color)
+		render.EndBeam()
 	end
 end
